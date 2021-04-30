262dacd5ad6d0f0f66ec3e90b70843079fc022fe
shadysankalp <83473069+shadysankalp@users.noreply.github.com>
2021-05-01 00:41:10 +0530

    Add files via upload

---
+++ sum of two nos.c
@@ -0,0 +1,13 @@
+//sum of two numbers and, if both are same return twice the sum
+#include<stdio.h>
+int main()
+{
+int a,b,c;   
+printf("Enter first no:\n");
+scanf("%d",&a);
+printf("Enter second no:\n ");
+scanf("%d",&b);
+c=a+b;
+printf("the sum is %d",c);
+return 0;
+}
